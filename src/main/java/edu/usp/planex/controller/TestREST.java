34cfff5a954f1c8b3e9996672b9cd8f33a70f111
giulianoprado <giulianoprado@gmail.com>
2017-08-07 01:56:51 -0300

    Added quote estimation

--- src/main/java/edu/usp/planex/controller/TestREST.java
+++ src/main/java/edu/usp/planex/controller/TestREST.java
@@ -4,11 +4,14 @@ package edu.usp.planex.controller;
  * Created by giulianoprado on 27/06/17.
  */
 
+import edu.usp.planex.business.CambioService;
 import edu.usp.planex.cambios.Cambio;
 import edu.usp.planex.cambios.CambioCotacao;
 import edu.usp.planex.cambios.CambioTurismo;
 import edu.usp.planex.cambios.CambioYahoo;
+import edu.usp.planex.dao.BacenDAO;
 import edu.usp.planex.dao.PriceDAO;
+import edu.usp.planex.model.Price;
 import edu.usp.planex.model.Provider;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,11 +31,14 @@ public class TestREST
     //@Autowired
     //PriceDAO priceDAO;
 
+
     @RequestMapping(value = "/api", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<String> test() {
+        double aaaa = new CambioService().calculateQuote("NUBANK","2017-08-08");
+        new BacenDAO().getQuote("2017-08-06");
         List<Provider> test = new PriceDAO().getProviderList();
 
-
+        List<Price> testt = new PriceDAO().getPricesByProvider("BB","2017-08-06");
         List<Cambio> cambios = new ArrayList<>();
         cambios.add(new CambioTurismo());
         cambios.add(new CambioYahoo());
@@ -52,6 +58,11 @@ public class TestREST
     public String addValue(@RequestParam(value = "value") double value, @RequestParam(value = "provider") String provider) {
         new PriceDAO().addPrice(value, provider);
         return "ok";
+    }
+
+    @RequestMapping(value = "/getQuote", method = RequestMethod.GET)
+    public double getQuote(@RequestParam(value = "date") String date, @RequestParam(value = "provider") String provider) {
+        return new CambioService().calculateQuote(provider, date);
     }
 
 
